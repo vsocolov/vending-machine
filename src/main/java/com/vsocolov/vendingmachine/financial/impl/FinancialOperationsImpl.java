fab71950fb61d88ba2e2b94bcfaa731c296243d4
Vladislav Socolov <socolov.vladislav@gmail.com>
2018-04-02 16:01:09 +0100

    added financial service

--- src/main/java/com/vsocolov/vendingmachine/financial/impl/FinancialOperationsImpl.java
+++ src/main/java/com/vsocolov/vendingmachine/financial/impl/FinancialOperationsImpl.java
@@ -4,15 +4,52 @@ import com.vsocolov.vendingmachine.data.Change;
 import com.vsocolov.vendingmachine.enums.Coin;
 import com.vsocolov.vendingmachine.financial.FinancialOperations;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class FinancialOperationsImpl implements FinancialOperations {
 
+    /**
+     * This list compute list of possible changes
+     *
+     * @param amount         amount to compute change
+     * @param availableCoins list of available coins
+     * @return a list of changes
+     */
     @Override
     public List<List<Change>> calculateChanges(final int amount, final List<Coin> availableCoins) {
         // sort coins descending
         availableCoins.sort((obj1, obj2) -> obj1.getAmount() < obj2.getAmount() ? 1 : -1);
 
-        return null;
+        final List<List<Change>> changes = new ArrayList<>();
+        for (int i = 0; i < availableCoins.size(); i++) {
+            final Coin coin = availableCoins.get(i);
+            if (coin.getAmount() <= amount) {
+                changes.add(doCalculate(i, amount, availableCoins));
+            }
+        }
+
+        return changes;
+    }
+
+    private List<Change> doCalculate(final int index, final int amount, final List<Coin> availableCoins) {
+        if (amount <= 0 || index >= availableCoins.size())
+            return Collections.emptyList();
+
+        final Coin coin = availableCoins.get(index);
+        int remainingAmount = amount;
+        int coinCounter = 0;
+
+        while (remainingAmount - coin.getAmount() >= 0) {
+            remainingAmount -= coin.getAmount();
+            coinCounter++;
+        }
+
+        final List<Change> changeList = new ArrayList<>();
+        changeList.add(new Change(coin, coinCounter)); //add current coin with quantity
+        changeList.addAll(doCalculate(index + 1, remainingAmount, availableCoins)); //add remaining coins
+
+        return changeList;
     }
 }
--- src/test/java/com/vsocolov/vendingmachine/financial/impl/FinancialOperationsImplTest.java
+++ src/test/java/com/vsocolov/vendingmachine/financial/impl/FinancialOperationsImplTest.java
@@ -1,18 +1,44 @@
 package com.vsocolov.vendingmachine.financial.impl;
 
+import com.vsocolov.vendingmachine.data.Change;
+import com.vsocolov.vendingmachine.enums.Coin;
 import com.vsocolov.vendingmachine.financial.FinancialOperations;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.List;
 
 import static com.vsocolov.vendingmachine.enums.Coin.*;
+import static org.hamcrest.CoreMatchers.hasItems;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.beans.HasPropertyWithValue.hasProperty;
 
 public class FinancialOperationsImplTest {
 
     private FinancialOperations financialOperations = new FinancialOperationsImpl();
 
     @Test
-    public void calculateChanges() {
-        financialOperations.calculateChanges(150, Arrays.asList(ONE_PENNY, FIVE_PENCE, TEN_PENCE, TWENTY_PENCE));
+    public void calculateChanges_should_return_a_list_of_possible_coin_computations() {
+        final String quantityProp = "quantity";
+        final String coinProp = "coin";
+        final List<Coin> coins = Arrays.asList(ONE_PENNY, FIVE_PENCE, TEN_PENCE, TWENTY_PENCE);
+
+        final List<List<Change>> changes = financialOperations.calculateChanges(15, coins);
+
+        assertThat(changes, hasSize(3));
+        assertThat(changes.get(0), hasItems(hasProperty(coinProp, equalTo(Coin.TEN_PENCE)), hasProperty(quantityProp, equalTo(1))));
+        assertThat(changes.get(0), hasItems(hasProperty(coinProp, equalTo(Coin.FIVE_PENCE)), hasProperty(quantityProp, equalTo(1))));
+        assertThat(changes.get(1), hasItems(hasProperty(coinProp, equalTo(Coin.FIVE_PENCE)), hasProperty(quantityProp, equalTo(3))));
+        assertThat(changes.get(2), hasItems(hasProperty(coinProp, equalTo(Coin.ONE_PENNY)), hasProperty(quantityProp, equalTo(15))));
+    }
+
+    @Test
+    public void calculateChanges_should_return_empty_list_if_its_not_possible_to_compute_changes() {
+        final List<Coin> coins = Arrays.asList(TWENTY_PENCE, ONE_POUND);
+
+        final List<List<Change>> changes = financialOperations.calculateChanges(15, coins);
+        assertThat(changes, is(empty()));
     }
 }
